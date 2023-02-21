380d55a9b6c104583c3cb0ae21939470b13728be
Tetiana Novitska <novittana2410@gmail.com>
2023-02-21 21:39:30 +0200

    final edits

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,12 +3,13 @@ import css from '../Modal/Modal.module.css'
 
 export class Modal extends Component {
   componentDidMount() {
-
     window.addEventListener('keydown', this.handleKeyDown);
+    console.log("Вмонтовано");
   }
 
   componentWillUnmount() {
     window.removeEventListener('keydown', this.handleKeyDown);
+    console.log("Видалено");
   }
 
   handleKeyDown = event => {
@@ -25,7 +26,7 @@ export class Modal extends Component {
 
   render() {
     return (
-      <div className={css.overlay} onClick={this.props.onCloseClick}>
+      <div className={css.overlay} onClick={this.handleBackdropClick}>
         <div className={css.modal}>
           <img src={this.props.src} alt="" />
         </div>
